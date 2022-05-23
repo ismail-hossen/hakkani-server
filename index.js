eb07c449edbc41bc41ac053a9cea935e27a756af
ismail-hossen <satict2016@gmail.com>
2022-05-23 17:38:59 +0600

    get specific user order

--- index.js
+++ index.js
@@ -1,42 +1,52 @@
-const express = require('express');
-const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
 const port = process.env.PORT || 5000;
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@hakkani-store.jbxar.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
 async function run() {
   try {
     await client.connect();
-    const TCollection = client.db('tools').collection('TCollection');
-    const OCollection = client.db('tools').collection('OCollection');
-    
-    app.get('/tools-collection', async (req, res) => {
+    const TCollection = client.db("tools").collection("TCollection");
+    const OCollection = client.db("tools").collection("OCollection");
+
+    // tools collection 
+    app.get("/tools-collection", async (req, res) => {
       const getData = await TCollection.find({}).toArray();
       res.send(getData);
     });
 
-    app.post('/order-collection', async (req, res) => {
-      const body = req.body
-      const orders = await OCollection.insertOne(body)
+    // order collection
+    app.post("/order-collection", async (req, res) => {
+      const body = req.body;
+      const orders = await OCollection.insertOne(body);
       res.send(orders);
     });
-
-  }
-  finally {
+    app.get("/order-collection/:email", async (req, res) => {
+      const email = req.params.email;
+      const orders = await OCollection.find({ email }).toArray();
+      res.send(orders);
+    });
+    
+  } finally {
   }
 }
 run().catch(console.dir);
 
-app.get('/', (req, res) => {
-  res.send('hello world!')
-})
+app.get("/", (req, res) => {
+  res.send("hello world!");
+});
 
 app.listen(port, () => {
-  console.log(`server running ${port}`)
-})
+  console.log(`server running ${port}`);
+});
