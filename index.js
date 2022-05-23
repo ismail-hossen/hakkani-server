81349b54c8504e134ddf7f368c469eac86df72cb
ismail-hossen <satict2016@gmail.com>
2022-05-23 16:56:20 +0600

    add post api for get order

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ require('dotenv').config();
 const port = process.env.PORT || 5000;
 const app = express();
 app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@hakkani-store.jbxar.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -13,10 +14,17 @@ async function run() {
   try {
     await client.connect();
     const TCollection = client.db('tools').collection('TCollection');
+    const OCollection = client.db('tools').collection('OCollection');
     
     app.get('/tools-collection', async (req, res) => {
-      const result = await TCollection.find({});
-      res.send(result);
+      const getData = await TCollection.find({}).toArray();
+      res.send(getData);
+    });
+
+    app.post('/order-collection', async (req, res) => {
+      const body = req.body
+      const orders = await OCollection.insertOne(body)
+      res.send(orders);
     });
 
   }
